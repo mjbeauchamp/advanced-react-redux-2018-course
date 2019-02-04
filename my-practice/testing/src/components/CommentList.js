3c6ef81bdf90b114dcce17850f90177911a42c3a
Monica Beauchamp <monica.j.beauchamp@gmail.com>
2019-02-04 16:29:11 -0700

    Basic testing app setup with App.test.js file

---
+++ my-practice/testing/src/components/CommentList.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+
+class CommentList extends Component {
+    render(){
+        return (
+            <div>
+                Comment List
+            </div>
+        )
+    }
+}
+
+export default CommentList;
