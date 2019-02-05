6e1aab93cf64c392021d860003363b34f09fa1d0
Monica Beauchamp <monica.j.beauchamp@gmail.com>
2019-02-05 13:55:18 -0700

    Testing set up

--- my-practice/testing/src/components/__tests__/App.test.js
+++ my-practice/testing/src/components/__tests__/App.test.js
@@ -3,5 +3,16 @@ import ReactDOM from 'react-dom'
 import App from '../App'
 
 it("shows a comment box", () => {
+    //Creates a "virtual" div in the virtual DOM that the testing environment creates
+    const div = document.createElement('div');
     
+    //Renders the App componenet into the div we created in the virtual DOM
+    ReactDOM.render(<App />, div)
+    console.log(div.innerHTML)
+    expect(div.innerHTML).toContain('Comment Box')
+
+
+    //Removes the App component from the virtual DOM
+    //This is cleanup after the test is run. We don't want to end up with leftover compoenents in the virtual DOM
+    ReactDOM.unmountComponentAtNode(div);
 })
